fe74458c1567f8743e4d012aaf785765f7e86908
Matthieu Poullet <matthieu.poullet@gmail.com>
2019-05-29 16:42:02 +0200

    Add uninstall PowerShell script.

---
+++ uninstall.ps1
@@ -0,0 +1,7 @@
+﻿$apps = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE InstallDate like '20160628'"
+# WHERE Name like '%Microsoft%' 
+
+foreach ($app in $apps) {
+    "Name = " + $app.name
+    $app.Uninstall()
+}
