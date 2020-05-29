4a093eb77f7d09513fd10f6380a048da9b290cab
Matthieu Poullet <matthieu.poullet@gmail.com>
2020-05-29 22:44:37 +0200

    Use hub sync if available.

--- rinse.sh
+++ rinse.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # https://gist.github.com/mpoullet/c2936060b56be394f9f04785b25d1510
+#set -x
+set -euo pipefail
+
 git clean -xfd
 git submodule foreach --recursive git clean -xfd
 git reset --hard
 git submodule foreach --recursive git reset --hard
 git submodule update --init --recursive
+if [ -x "$(command -v hub)" ]; then
+    hub sync
+fi
