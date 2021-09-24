c2998ade19682f9e22e292a87c2223ff43f7814c
Matthieu Poullet <m.poullet@avm.de>
2021-09-24 12:51:35 +0200

    Add LFS cleanup
    - add function rinse
    - add function lfs
    - add function main for control flow

--- rinse.sh
+++ rinse.sh
@@ -3,23 +3,49 @@
 #set -x
 set -euo pipefail
 
-# Clean main repo
-git clean -xfd
-# Clean all submodules
-git submodule foreach --recursive git clean -xfd
-# Reset main repo
-git reset --hard
-# Reset all submodules
-git submodule foreach --recursive git reset --hard
-# Sync main repo
-if [ -x "$(command -v hub)" ]; then
-    hub sync
-else
-    git fetch --prune
-fi
-# Sync all submodules
-git submodule sync --recursive
-# Update main repo
-git pull --ff-only
-# Update all submodules
-git submodule update --init --recursive
+function rinse() {
+    # Clean main repo
+    git clean -xfd
+    # Clean all submodules
+    git submodule foreach --recursive git clean -xfd
+    # Reset main repo
+    git reset --hard
+    # Reset all submodules
+    git submodule foreach --recursive git reset --hard
+    # Sync main repo
+    if [ -x "$(command -v hub)" ]; then
+        hub sync
+    else
+        git fetch --prune
+    fi
+    # Sync all submodules
+    git submodule sync --recursive
+    # Update main repo
+    git pull --ff-only
+    # Update all submodules
+    git submodule update --init --recursive
+}
+
+function lfs() {
+    git rm --cached -r .
+    git reset --hard
+    git rm .gitattributes
+    git reset .
+    git checkout .
+    git lfs uninstall
+    git reset --hard
+    git lfs install
+    git lfs pull
+}
+
+function main() {
+    local -r arg1=${1:-}
+
+    if [ "${arg1}" == "lfs" ]; then
+        lfs
+    fi
+
+    rinse
+}
+
+main "$@"
